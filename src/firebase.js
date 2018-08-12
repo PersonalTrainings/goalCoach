7c9fbe00befcabba441f5fbdc6dc098573101083
Narek Khachatryan <narek1993x@mail.ru>
2018-08-12 15:54:46 +0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,6 @@
+import * as firebase from 'firebase';
+import config from '../config/fb.json'
+
+export const firebaseApp = firebase.initializeApp(config);
+export const goalRef = firebase.database().ref('goals');
+export const completeGoalRef = firebase.database().ref('completeGoals');
